f7df3c7c6d541a8c059142830bead9350d5c5555
Arkadii <arkady2003@gmail.com>
2025-08-06 22:42:22 +0400

    Add more commands for VEQ1656 remote. Use map to store commands & codes

--- RS232_to_IR/main.c
+++ RS232_to_IR/main.c
@@ -14,17 +14,100 @@
 #include "uart.h"
 #include "ir_send.h"
 
-volatile bool ir_trigger = false;
+// Panasonic remote address
+#define ADDRESS		0x4004
+
+// RS-232 commands
+#define PLAY_COMMAND			0x3A
+#define STOP_COMMAND			0x3F
+#define FF_COMMAND				0xAB
+#define REW_COMMAND				0xAC
+
+#define PAUSE_COMMAND			0x3B
+#define EJECT_COMMAND			0x2A
+#define REC_COMMAND				0x3C
+#define SLOW_COMMAND			0xB1
+
+#define MENU_COMMAND			0xE0
+#define OSD_COMMAND				0xE1
+
+#define TRACKING_MINUS_COMMAND	0xD1
+#define TRACKING_PLUS_COMMAND	0xD2
+
+#define KEY_DOWN_COMMAND		0xC1
+#define KEY_STORE_COMMAND		0xC2
+
+// IR codes for VEQ1656
+#define PLAY_CODE		0x09005059
+#define STOP_CODE		0x09000009	// eject is possible by holding down STOP button on a remote
+#define FF_CODE			0x0900C0C9
+#define REW_CODE		0x09004049
+
+#define PAUSE_CODE		0x09006069
+#define SLOW_CODE		0x0900F0F9	// slow mode
+#define REC_CODE		0x09001019
+
+#define MENU_CODE		0x09806AE3
+#define OSD_CODE		0x0900EAE3
+
+#define TRACKING_MINUS	0x098040C9 // tracking - | key UP
+#define TRACKING_PLUS	0x098066EF // tracking +
+
+#define KEY_DOWN		0x0980C049
+#define KEY_STORE		0x09801A93 // store/OK | reset
+
+typedef struct {
+	uint8_t command;
+	uint32_t irCode;
+} CommandMap;
+
+const CommandMap commandMap[] = {
+	{ PLAY_COMMAND,            PLAY_CODE },
+	{ STOP_COMMAND,            STOP_CODE },
+	{ FF_COMMAND,              FF_CODE },
+	{ REW_COMMAND,             REW_CODE },
+	{ PAUSE_COMMAND,           PAUSE_CODE },
+	{ EJECT_COMMAND,           STOP_CODE },  // same IR as STOP
+	{ REC_COMMAND,             REC_CODE },
+	{ SLOW_COMMAND,            SLOW_CODE },
+	{ MENU_COMMAND,            MENU_CODE },
+	{ OSD_COMMAND,             OSD_CODE },
+	{ TRACKING_MINUS_COMMAND,  TRACKING_MINUS },
+	{ TRACKING_PLUS_COMMAND,   TRACKING_PLUS },
+	{ KEY_DOWN_COMMAND,        KEY_DOWN },
+	{ KEY_STORE_COMMAND,       KEY_STORE }
+};
+
+uint8_t cmd;
 
 ISR(USART_RX_vect) {
-	switch(UDR0){
-		default:	
-		ir_trigger = true;
-		break;
+	cmd = UDR0;
+	for (uint8_t i = 0; i < sizeof(commandMap) / sizeof(commandMap[0]); i++) {
+		if (commandMap[i].command == cmd) {
+			sendPanasonic(ADDRESS, commandMap[i].irCode);
+			break;
+		}
 	}
-	
-	//ir_trigger = true;
-
+// 	switch(UDR0){
+// 		case PLAY_COMMAND:
+// 			sendPanasonic(ADDRESS, PLAY_CODE);
+// 			break;
+// 		case STOP_COMMAND:
+// 			sendPanasonic(ADDRESS, STOP_CODE);
+// 			break;
+// 		case FF_COMMAND:
+// 			sendPanasonic(ADDRESS, FF_CODE);
+// 			break;
+// 		case REW_COMMAND:
+// 			sendPanasonic(ADDRESS, REW_CODE);
+// 			break;
+// 		case PAUSE_COMMAND:
+// 			sendPanasonic(ADDRESS, PAUSE_CODE);
+// 			break;
+// 		case EJECT_COMMAND:
+// 			sendPanasonic(ADDRESS, STOP_CODE);
+// 			break;
+// 	}
 }
 
 int main(void)
@@ -41,14 +124,7 @@ int main(void)
 	
     while (1) 
     {
-		if (ir_trigger)
-		{
-			ir_trigger = false;
 
-			sendPanasonic(0x4004, 0x09005059);
-			_delay_ms(100);
-
-	    }
 	}
 }
 
